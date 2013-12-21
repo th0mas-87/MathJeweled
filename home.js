e1ad1899ca3c1ca4d6e0034eba241f27abc782b4
th0mas-87 <epic.th0mas87@gmail.com>
2013-12-20 17:15:10 -0700

    Nav changes

--- home.js
+++ home.js
@@ -9,11 +9,12 @@ $(document).ready(function() {
 	$('span#h2').css('-webkit-animation-play-state', 'running');
 	$('nav').css('animation-play-state', 'running');
 	$('nav').css('-webkit-animation-play-state', 'running');
+	$('nav').css('width', $(window).width() * 0.85 + 'px');
 	$('nav').css('top', $('span#h2').position().top + $(window).width() / 12 + 'px');
-	$('nav a').css('font-size', $(window).width() / 35 + 'px');
+	$('nav a').css('font-size', $(window).width() / 50 + 'px');
 	$('.fancybox-media').fancybox({
-		openEffect  : 'none',
-		closeEffect : 'none',
+		openEffect  : 'fade',
+		closeEffect : 'fade',
 		helpers : {
 			media : {}
 		}
@@ -21,8 +22,8 @@ $(document).ready(function() {
 });
 
 $(window).resize(function() {
-	$('*').css('transition', '0.5s ease');
-	$('*').css('-webkit-transition', '0.5s ease');
+	$('*').css('transition', '0.25s ease');
+	$('*').css('-webkit-transition', '0.25s ease');
 	setTimeout(function() {
 		$('span#h1').css('font-size', $(window).width() / 7 + 'px');
 		$('span#h2').css('font-size', $(window).width() / 45 + 'px');
@@ -30,12 +31,13 @@ $(window).resize(function() {
 			$('span#h2').css('top', $('span#h1').height() + 15 + 'px');
 			setTimeout(function() {
 				$('nav').css('top', $('span#h2').position().top + $(window).width() / 12 + 'px');
-				$('nav a').css('font-size', $(window).width() / 35 + 'px');
+				$('nav').css('width', $(window).width() * 0.85 + 'px');
+				$('nav a').css('font-size', $(window).width() / 50 + 'px');
 				setTimeout(function() {
 					$('*').css('transition', 'none');
 					$('*').css('-webkit-transition', 'none');
-				}, 750);
-			}, 750);
-		}, 750);
-	}, 500);
+				}, 250);
+			}, 250);
+		}, 250);
+	}, 250);
 });
